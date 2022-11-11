adf352c91934a91a875d645187f4ca44088c27eb
Martin Fassi <fassim@icloud.com>
2022-11-11 10:00:04 +0100

    FEATURE - retry script improvements

--- scripts/deploy/test_retry.sh
+++ scripts/deploy/test_retry.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 set -x
 set +e
-
+# test
 trap "exit 1" TERM
-# Get main script pid so we can kill it from within a function
 export MAIN_PID=$$
 
+# Get main script pid so we can kill it from within a function
 ssh_command () {
     ssh $1 $2
     exit_code=$?
     if [ ! $exit_code -eq 0 ]
     then
+        >&2 echo "Command failed"
         kill -s TERM $MAIN_PID
     fi
 }
                     
-if [ $retryCount -gt 1 ]
+if [ $1 -gt 1 ]
 then
-    echo "RETRY NUMBER ${retryCount}"
+    echo "RETRY NUMBER $1"
 fi
 if [ $i -eq 1 ]
 then
